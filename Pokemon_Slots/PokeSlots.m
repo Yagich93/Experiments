b225ae874df03fa703f79fa09add31d63aaeb0a1
Yagich <yagich@bk.ru>
2016-02-28 18:34:53 +0300

    Add random card deck guessing.
    Add cash.
    Add iteration payment.
    Add random guessing.
    Add rewards.

--- Pokemon_Slots/PokeSlots.m
+++ Pokemon_Slots/PokeSlots.m
@@ -5,11 +5,19 @@ close all;
 
 % Slot initial variables.
 initCards = [12 12];
+cash = 0;
 
 % Card loop.
 cards = initCards;
 for i = 1:12
+    % Pay for iteration.
+    cash = cash - 3;
+    % Pick card.
     deck = randi(2);
     cards(deck) = cards(deck) - 1;
-    disp(cards);
+    % Guess card.
+    guess = randi(2);
+    cash = cash + 6*(deck == guess);
+    % Display stats.
+    disp(cash);
 end
