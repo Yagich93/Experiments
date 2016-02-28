1d77f6d2b0c6bab435c61e11e1195159082600b3
Yagich <yagich@bk.ru>
2016-02-28 18:46:40 +0300

    Add cash history.
    Add game reiteration.
    Add cash history plotting for random playing.
    Add variable names.

--- Pokemon_Slots/PokeSlots.m
+++ Pokemon_Slots/PokeSlots.m
@@ -3,21 +3,34 @@ clc;
 clearvars;
 close all;
 
-% Slot initial variables.
-initCards = [12 12];
-cash = 0;
+% Params.
+N = 10000;
+deckSize = 12;
+cost = 3;
 
-% Card loop.
-cards = initCards;
-for i = 1:12
-    % Pay for iteration.
-    cash = cash - 3;
-    % Pick card.
-    deck = randi(2);
-    cards(deck) = cards(deck) - 1;
-    % Guess card.
-    guess = randi(2);
-    cash = cash + 6*(deck == guess);
-    % Display stats.
-    disp(cash);
+% Slot initial variables.
+initCards = [deckSize deckSize];
+cash = 0;
+cashHistory = zeros(N*deckSize, 1);
+
+% Game iterations.
+round = 0;
+for j = 1:N
+    % Card loop.
+    cards = initCards;
+    for i = 1:deckSize
+        round = round + 1;
+        % Pay for iteration.
+        cash = cash - cost;
+        % Pick card.
+        deck = randi(2);
+        cards(deck) = cards(deck) - 1;
+        % Guess card.
+        guess = randi(2);
+        cash = cash + 2*cost*(deck == guess);
+        cashHistory(round) = cash;
+    end
 end
+
+% Plotting.
+plot(cashHistory);
