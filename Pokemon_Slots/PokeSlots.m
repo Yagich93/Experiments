70960c775afc4f43c571adf5fc432890a0b2a6d7
Yagich <yagich@bk.ru>
2016-02-28 19:01:37 +0300

    Add smart guessing.
    Add smart guessing (choosing the deck with more cards).
    Add smart cash history.
    Fix card picking.

--- Pokemon_Slots/PokeSlots.m
+++ Pokemon_Slots/PokeSlots.m
@@ -4,7 +4,7 @@ clearvars;
 close all;
 
 % Params.
-N = 10000;
+N = 1000;
 deckSize = 12;
 cost = 3;
 
@@ -12,6 +12,8 @@ cost = 3;
 initCards = [deckSize deckSize];
 cash = 0;
 cashHistory = zeros(N*deckSize, 1);
+smartCash = 0;
+smartCashHistory = zeros(N*deckSize, 1);
 
 % Game iterations.
 round = 0;
@@ -22,15 +24,25 @@ for j = 1:N
         round = round + 1;
         % Pay for iteration.
         cash = cash - cost;
+        smartCash = smartCash - cost;
         % Pick card.
-        deck = randi(2);
-        cards(deck) = cards(deck) - 1;
-        % Guess card.
+        card = randi(sum(cards));
+        deck = 1 + (card > cards(1));
+        % Guess card randomly.
         guess = randi(2);
         cash = cash + 2*cost*(deck == guess);
         cashHistory(round) = cash;
+        % Guess card in a smart way.
+        guess = 1 + (cards(2) > cards(1));
+        smartCash = smartCash + 2*cost*(deck == guess);
+        smartCashHistory(round) = smartCash;
+        % Remove card.
+        cards(deck) = cards(deck) - 1;
     end
 end
 
 % Plotting.
+hold on;
 plot(cashHistory);
+plot(smartCashHistory);
+hold off;
